36b84e4340e57fb81b13925d1fb2211b9b2ff156
yanagiragi <qaz90473@gmail.com>
2017-02-17 14:31:09 +0800

    Proj CleanUp & Output Properties to XML

--- Model.cs
+++ Model.cs
@@ -11,14 +11,12 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Data;
 using System.Data.Odbc;
 using System.Windows.Forms;
 
 namespace Bentley.ODBC.Sample
-    {
+{
     public class Model 
         {
         private OdbcConnection connection;
@@ -30,19 +28,20 @@ namespace Bentley.ODBC.Sample
         public Model (string fileName)
         {
             this.fileName = fileName;
-            foreach(string s in Schemas)
-            {
-                Console.WriteLine(s);
-            }
-            Console.WriteLine("Done.");
-            
+            ypDumpProperties();
+        }
+        
+        private void ypDumpProperties()
+        {
+            Data.WriteXml(this.fileName + ".xml");
+
             // Bad Ways But Works
             Environment.FailFast("Done");
+            // Alternative way :
             // System.Diagnostics.Process.GetCurrentProcess().Kill();
         }
-        //
+
         //Demonstrate how to make a connection to i-model
-        //
         private OdbcConnection Connection
         {
             get
@@ -56,27 +55,21 @@ namespace Bentley.ODBC.Sample
                 return connection;
             }
         }
-        //
+        
         //Close the connection when we are done
-        //
         public void Close ()
         {
             connection.Close ();
         }
 
-        //
         //Setter of i-model file name. Each i-model file is modeled as a database category.
         //The name of the category is a normalized verion of the file name.
-        //
         public string FileName
         {
-            get
-            {
-            return fileName;
+            get {
+                return fileName;
             }
-
-            set
-            {
+            set {
                 if (fileName != value) {
                     if (connection != null && connection.State== ConnectionState.Open)
                         connection.Close ();
@@ -86,107 +79,92 @@ namespace Bentley.ODBC.Sample
             }
         }
 
-        //
         //Demonstrate how to get all the schemas of an i-model
-        //
         public List<string> Schemas
-            {
-            get
-                {
+        {
+            get {
                 List<string> schamas = new List<string> ();
                 DataTable tables = Connection.GetSchema ("Tables");
 
                 //Get catagory
-                if (tables.Rows.Count > 0)
-                    {
+                if (tables.Rows.Count > 0) {
                     DBCatagory = tables.Rows[0]["TABLE_CAT"] as string;
-                    }
-                foreach (DataRow r in tables.Rows)
-                    {                    
-                    if (!schamas.Contains (r["TABLE_SCHEM"] as string))
-                        {
-                        schamas.Add (r["TABLE_SCHEM"] as string);
-                        }
-                    }
-                return schamas;
                 }
+                foreach (DataRow r in tables.Rows)
+                {                    
+                    if (!schamas.Contains (r["TABLE_SCHEM"] as string)) {
+                        schamas.Add (r["TABLE_SCHEM"] as string);
+                    }
+                }
+                return schamas;
             }
+        }
 
-
-        //
         //Demonstrate how to get all the tables of a given schema
-        //
         public List<string> GetTableNames (string schema)
-            {
+        {
             List<string> retVal = new List<string> ();
             DataTable tables = Connection.GetSchema ("Tables", new string[] { null, schema });
+
             foreach (DataRow r in tables.Rows)
                 retVal.Add (r["TABLE_NAME"] as string);
 
             return retVal;
-            }
+        }
 
-        //
         //Get all the fields of a table
-        //
         public List<string> GetFields (string schema, string table)
-            {
+        {
             List<string> retVal = new List<string> ();
             DataTable tables = Connection.GetSchema ("Columns", new string[] { null, schema, table });
+
             foreach (DataRow r in tables.Rows)
                 retVal.Add (r["COL_NAME"] as string);
-            return retVal;
-            }
 
-        //
+            return retVal;
+        }
+
         //Using sql statement to get data. Make sure table name it the command text is in the format of "Category.schema.tableName".
-        //
         public DataTable GetView (string viewName, string commandstr)
-            {
+        {
             DataSet ds = new DataSet ();
             ds.EnforceConstraints = false;
 
             DataTable dt = new DataTable (viewName);
             ds.Tables.Add (dt);
             
-            try
-                {
+            try {
                 OdbcCommand command = new OdbcCommand (commandstr, Connection);
                 OdbcDataReader reader = command.ExecuteReader ();     //(command, Connection);
                 
                 dt.Load (reader);
                 reader.Close ();
-                }
-            catch (Exception e)
-                {
-                MessageBox.Show (e.Message);
-                }
-            return dt;
             }
-
-        //
-        //Retrieve all data from the model
-        //
-        public DataSet Data
+            catch (Exception e)
             {
-            get 
-                {
+                MessageBox.Show (e.Message);
+            }
+            return dt;
+        }
+
+        //Retrieve all data from the model
+        public DataSet Data
+        {
+            get {
                 string command = "select * from ";
                 DataSet retVal = new DataSet ();
-                foreach (string schema in Schemas)
-                    {
+                foreach (string schema in Schemas) {
                     List<string> tables = GetTableNames(schema);
-                    foreach (string table in tables)
-                        {
+                    foreach (string table in tables) {
                         string commandstr = command + DBCatagory + "." + schema + "." + table;
                         OdbcDataAdapter adapter = new OdbcDataAdapter(commandstr, Connection);
                         DataTable dt = new DataTable (table);
                         adapter.Fill (dt);
                         retVal.Tables.Add (dt);
-                        }
                     }
-                return retVal;
                 }
+                return retVal;
             }
-        }
+       }
     }
+}
--- Program.cs
+++ Program.cs
@@ -9,9 +9,6 @@
 |
 +--------------------------------------------------------------------------------------*/
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
 
 namespace Bentley.ODBC.Sample
 {
@@ -23,24 +20,7 @@ namespace Bentley.ODBC.Sample
         [STAThread]
         static void Main ()
         {
-            /*Application.EnableVisualStyles ();
-            Application.SetCompatibleTextRenderingDefault (false);
-            Application.Run (new imodelAccessForm ());*/
-
-            imodelAccessForm m = new imodelAccessForm();
-
-            Console.WriteLine("hi");
-
-            return;
-            //model = new Model(dialog.FileName);
-            //m.dump();
-
-            /*string filename = "D:\\_Course\\_MicroStation\\Models\\Unit\\pipe\\rd_02.i.dgn";
-            Model m = new Model(filename);
-            Environment.Exit(0);
-            //List<String> schemas = m.Schemas;*/
-
-            //string s = Console.ReadLine();
+            Model m = new Model("D:\\_Course\\_MicroStation\\Models\\Unit\\pipe\\rd_02.i.dgn");
         }
     }
 }
